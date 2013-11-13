59ba8ededc87402ed5d2b69cd11992e076c6e1d3
Philip Källberg <philip@cloud66.com>
2013-11-13 13:46:11 +0000

    Update database.rb

--- config/database.rb
+++ config/database.rb
@@ -1,6 +1,6 @@
-if ENV['MONGOHQ_URL']
-  uri = URI.parse(ENV['MONGOHQ_URL'])
-  MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'], :logger => logger)
+if ENV['MONGODB_ADDRESS']
+  uri = URI.parse(ENV['MONGODB_ADDRESS'])
+  MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGODB_ADDRESS'], :logger => logger)
   MongoMapper.database = uri.path.gsub(/^\//, '')
 else
   MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger)
